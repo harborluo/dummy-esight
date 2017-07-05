cf88b38abb8d533b443335f9e4cba17015602397
harbor <hluo@bdna.com>
2017-07-05 15:00:25 +0800

    add RestController annotation

--- src/main/java/com/esight/controller/IndexController.java
+++ src/main/java/com/esight/controller/IndexController.java
@@ -3,10 +3,12 @@ package com.esight.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by harbor on 6/21/2017.
  */
+@RestController
 public class IndexController {
 
     @RequestMapping("/")
